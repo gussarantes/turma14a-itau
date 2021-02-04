5afa704667e4c938c7427b4e4bbc40a970b152b9
gussarantes <gussarantes@hotmail.com>
2021-02-04 15:30:47 -0300

    colocando codigo

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
 <h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
 
-<h3> Um deploy aotomático feito com alunos do itaú </h3>
-
-<h5>Tentar sempre, desistir jamais!!!<h5>
-
 <div style="display:flex">
 	<div style="width:250px;height:250px;background:gray; padding:20px; margin:5px">
  	 Tiger 800
      <br>
       <img src="moto2.jpg" style="width: 200px">
+     <br>
+    R$ 39.999,90
+    <br>
+    <a href="" target="_blank" style="display:block;background:green; color:white; text-align:center; padding: 20px; border-radius:10px"> Comprar agora </a>
+    <br>
+    <a href="" target="_blank" >Entrar em contato</a>
+    <br>
+
 	</div>
 	<div style="width:250px;height:250px;background:gray;color:black; padding:20px; margin:5px">
   Tiger 800
       <br>
       <img src="moto1.jpg" style="width: 200px">
+      <br>
+    R$ 39.999,90
+    <br>
+    <a href="" target="_blank" style="display:block;background:green; color:white; text-align:center; padding: 20px; border-radius:10px"> Comprar agora </a>
+    <br>
+    <a href="" target="_blank" >Entrar em contato</a>
+    <br>
 	</div>
 </div>	

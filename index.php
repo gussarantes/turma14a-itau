45042135c654b78636fdb5655909625c46ce714e
gussarantes <gussarantes@hotmail.com>
2021-02-04 15:13:19 -0300

    colocando codigo

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
 <h5>Tentar sempre, desistir jamais!!!<h5>
 
 <div style="display:flex">
-	<div style="width:230px;height:120px;background:green; padding:20px; margin:5px">
- 	 Um teste em um retangulo
+	<div style="width:250px;height:250px;background:gray; padding:20px; margin:5px">
+ 	 Tiger 800
      <br>
-      <img src="moto2.jpg" style="width: 100px">
+      <img src="moto2.jpg" style="width: 200px">
 	</div>
-	<div style="width:230px;height:120px;background:red;color:white; padding:20px; margin:5px">
-	  Um teste em um retangulo
+	<div style="width:250px;height:250px;background:gray;color:black; padding:20px; margin:5px">
+  Tiger 800
       <br>
-      <img src="moto1.jpg" style="width: 100px">
+      <img src="moto1.jpg" style="width: 200px">
 	</div>
 </div>	
